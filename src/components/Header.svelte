4122bfdb7a62f357d19f93ced88de0eba8626f4d
Asif <73013715+gkbreload@users.noreply.github.com>
2025-04-25 07:37:17 +0700

    Add files via upload

---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { openModal } from "../store";
+</script>
+
+<header class="flex flex-col relative z-20">
+  <div
+    class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6"
+  >
+    <a href="/">
+      <h1 class="font-semibold">
+        Swoley <span class="text-indigo-400">Moley</span>
+      </h1>
+    </a>
+    <!-- svelte-ignore a11y_consider_explicit_label -->
+    <button
+      on:click={() => ($openModal = true)}
+      class="md:hidden grid place-items-center"
+    >
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a
+        href="#product"
+        class="duaration-200 hover:text-indigo-400 cursor-pointer">Product</a
+      >
+      <a
+        href="#reviews"
+        class="duaration-200 hover:text-indigo-400 cursor-pointer">Review</a
+      >
+      <a href="#faqs" class="duaration-200 hover:text-indigo-400 cursor-pointer"
+        >FAQs</a
+      >
+      <button class="specialBtn">
+        <p>Start free today</p>
+      </button>
+    </nav>
+  </div>
+</header>
