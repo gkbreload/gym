ca841697d29203d78607f25a13ac1e98ef488b6c
Asif <73013715+gkbreload@users.noreply.github.com>
2025-04-25 15:26:05 +0700

    Update Product.svelte

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -40,7 +40,7 @@
 
 <SectionWraper id="product">
   <div
-    class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14"
+    class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center pb-10 md:pb-14"
   >
     <div class="flex flex-col gap-2">
       <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
